ccc3b074f55a0b9d92b6d4bdb16d7956325427c1
ashutosh <ashumantoo@gmail.com>
2019-12-25 15:59:43 +0530

    outputting burger Ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,21 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    /**====================================================================
+     * The Ingredient data which is passed from the BurgerBuilder Component
+     * is an Object, not an array so we need to transform into an array
+     */
+    const transformedIngredient = Object.keys(props.ingredients)
+        .map(ingKey => {
+            //ingkey = salad,bacon,cheese,meat
+            return [...Array(props.ingredients[ingKey])].map((_, i) => {
+                return <BurgerIngredient key={ingKey + i} type={ingKey} />;
+            })
+        });
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredient}
             <BurgerIngredient type="bread-bottom" />
         </div>
     )
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,29 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+
+    //We can also create state using the constructor but we need to always
+    //call the super() inside the constructor
+    // constructor(props){
+    //     super(props);
+    //     this.state = {...};
+    // }
+
+    //creating the ingredients data using the state
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
     render() {
         return (
             <Aux>
-                <Burger />
+                {/**Passing ingredient data to the Burger component */}
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </Aux>
         );
