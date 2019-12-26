b0c8237dd728016a79b98c09021e0b313b9c38c8
ashutosh <ashumantoo@gmail.com>
2019-12-26 16:36:23 +0530

    Order summary Modal backdrop is added.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -77,9 +77,12 @@ class BurgerBuilder extends Component {
     }
 
     purchaseHandler = () => {
-        this.setState({ purchasing: true })
+        this.setState({ purchasing: true });
     }
 
+    purchaseCancelHandler = () => {
+        this.setState({ purchasing: false });
+    }
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -89,7 +92,7 @@ class BurgerBuilder extends Component {
         }
         return (
             <Aux>
-                <Modal show={this.state.purchasing}>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary ingredients={this.state.ingredients} />
                 </Modal>
                 {/**Passing ingredient data to the Burger component */}
