f5f0a5f3e38ff41f5584fd4b35c4af9f274fca70
ashutosh <ashumantoo@gmail.com>
2020-01-05 19:15:42 +0530

    fetching and displaying the orders from server.

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react';
+import axios from '../../axios-orders';
 
 import Order from '../../components/Order/Order';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(res => {
+                console.log(res.data);
+                const fetchedOrders = [];
+                for (const key in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                this.setState({ loading: false, orders: fetchedOrders });
+            })
+            .catch(error => {
+                this.setState({ loading: false });
+            });
+    }
+
     render() {
         return (
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => (
+                    <Order
+                        key={order.id}
+                        ingredients={order.Ingredients}
+                        price={order.price}
+                    />
+                ))}
             </div>
         )
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
