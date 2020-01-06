60fe2fab06e075dc358991a8738a3d00d9ac397f
ashutosh <ashumantoo@gmail.com>
2020-01-06 18:04:19 +0530

    dynamically creating the form input using js form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -9,11 +9,57 @@ import axios from '../../../axios-orders';
 class ContactData extends Component {
 
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP Code'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your E-Mail'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        { value: 'fastest', displayValue: 'Fastest' },
+                        { value: 'cheapest', displayValue: "Cheapest" }
+                    ]
+                },
+                value: ''
+            }
         },
         loading: false
     }
@@ -26,16 +72,6 @@ class ContactData extends Component {
         const order = {
             Ingredients: this.props.ingredients,
             price: this.props.price,
-            customer: {
-                name: 'Ashutosh',
-                address: {
-                    street: '7th Main',
-                    zipCode: '560075',
-                    country: 'India'
-                },
-                email: 'ashu@test.com'
-            },
-            deliveryMethod: 'fastest'
         }
         axios.post('/orders.json', order).then(response => {
             this.setState({ loading: false });
@@ -46,12 +82,31 @@ class ContactData extends Component {
         });
     }
     render() {
+        const formElementArray = [];
+        for (let key in this.state.orderForm) {
+            formElementArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+
         let form = (
+            // <form>
+            //     <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
+            //     <Input inputtype="input" type="email" name="email" id="" placeholder="Your Email" />
+            //     <Input inputtype="input" type="text" name="street" id="" placeholder="Street" />
+            //     <Input inputtype="input" type="text" name="postalCode" id="" placeholder="Postal Code" />
+            //     <Button btnType="Success" clicked={this.orderHandler}>Order </Button>
+            // </form>
             <form>
-                <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
-                <Input inputtype="input" type="email" name="email" id="" placeholder="Your Email" />
-                <Input inputtype="input" type="text" name="street" id="" placeholder="Street" />
-                <Input inputtype="input" type="text" name="postalCode" id="" placeholder="Postal Code" />
+                {formElementArray.map(formElement => (
+                    <Input
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}
+                    />
+                ))}
                 <Button btnType="Success" clicked={this.orderHandler}>Order </Button>
             </form>
         );
