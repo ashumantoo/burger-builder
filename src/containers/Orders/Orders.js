beeb9fe398b90f8fe9d95f6a9765de0a2db848b6
ashutosh <ashumantoo@gmail.com>
2020-01-13 22:35:49 +0530

    sending the token with the http request .

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -33,7 +33,7 @@ class Orders extends Component {
         //         this.setState({ loading: false });
         //     });
 
-        this.props.onFetchOrders();
+        this.props.onFetchOrders(this.props.token);
     }
 
     render() {
@@ -58,13 +58,14 @@ class Orders extends Component {
 const mapStateToProps = state => {
     return {
         orders: state.order.orders,
-        loading: state.order.loading
+        loading: state.order.loading,
+        token: state.auth.token
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onFetchOrders: () => dispatch(actions.fetchOrders())
+        onFetchOrders: (token) => dispatch(actions.fetchOrders(token))
     }
 }
 
